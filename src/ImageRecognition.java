667d5eb7f7d504748554f3778438b7c605905354
seiyaa <seiyamiro@163.com>
2017-07-01 16:45:44 +0800

    WIP:surf

---
+++ src/ImageRecognition.java
@@ -0,0 +1,31 @@
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.features2d.FeatureDetector;
+
+
+public class ImageRecognition {
+    private float nndrRation = 0.7f;
+    private int matchPointCount = 0;
+    
+    public void matchImage(Mat template, Mat origin) {
+        MatOfKeyPoint templateKeyPoints = new MatOfKeyPoint();
+        FeatureDetector featureDetector = FeatureDetector.create(FeatureDetector.SURF);
+        
+        
+    }
+    public float getNndrRation() {
+        return nndrRation;
+    }
+    public void setNndrRation(float nndrRation) {
+        this.nndrRation = nndrRation;
+    }
+    public int getMatchPointCount() {
+        return matchPointCount;
+    }
+    public void setMatchPointCount(int matchPointCount) {
+        this.matchPointCount = matchPointCount;
+    }
+    public float getNndrRatio() {
+        
+    }
+}
